de5993ddb1cc333196fbeeb2b7f6546579e6ad85
EmersonChipana <emerson.chipana@ucb.edu.bo>
2022-12-01 01:15:02 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'proyecto-iot-fc433',
+    appId: '1:119991346425:web:537da32a43f1aa53dd2e28',
+    databaseURL: 'https://proyecto-iot-fc433-default-rtdb.firebaseio.com',
+    storageBucket: 'proyecto-iot-fc433.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAqm2_V-RZMAB6e8R7NjNiD8J826ZrHPmc',
+    authDomain: 'proyecto-iot-fc433.firebaseapp.com',
+    messagingSenderId: '119991346425',
+    measurementId: 'G-DD767TFN64',
+  },
   production: true
 };
